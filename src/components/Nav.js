ddb5f52858e3de6557e4efcf369af09fe1e94908
lulubun <laurapetitt@gmail.com>
2017-03-05 01:51:51 -0500

    still debugging

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,21 +2,24 @@ import React from 'react';
 import {Link} from 'react-router';
 import Weather from './Weather'
 
-export default function Nav() {
-  render()
-  return (
-    <div>
+export class Nav extends React.Component {
+  render() {
+    return(
       <div>
-        <nav className="topBar">
-          <ul>
-            <li><Link to={'/'}>Workday Weather</Link></li>
-            <li><Link to={'/recommendations'}>What Should I Wear Today?</Link></li>
-          </ul>
-        </nav>
+        <div>
+          <nav className="topBar">
+            <ul>
+              <li><Link to={'/'}>Workday Weather</Link></li>
+              <li><Link to={'/recommendations'}>What Should I Wear Today?</Link></li>
+            </ul>
+          </nav>
+        </div>
+        <div>
+          <Weather />
+        </div>
       </div>
-      <div>
-        <Weather />
-      </div>
-    </div>
-  )
+    );
+  }
 }
+
+export default Nav
